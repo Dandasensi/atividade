aa42e8b4158f9072d0ae7aa4c4d384e0a3a01df4
Daniel <136037335+Dandasensi@users.noreply.github.com>
2023-06-12 10:16:00 -0300

    Update questao1.php
    .

---
+++ questao1.php
@@ -0,0 +1,25 @@
+<?php
+
+$valor1 = 12;
+$valor2 = 6;
+
+// Adição
+$resultado = $valor1 + $valor2;
+echo "Adição: " . $resultado . "<br>";
+
+// Subtração
+$resultado = $valor1 - $valor2;
+echo "Subtração: " . $resultado . "<br>";
+
+// Divisão
+$resultado = $valor1 / $valor2;
+echo "Divisão: " . $resultado . "<br>";
+
+// Multiplicação
+$resultado = $valor1 * $valor2;
+echo "Multiplicação: " . $resultado . "<br>";
+
+// Resto da divisão
+$resultado = $valor1 % $valor2;
+echo "Resto da divisão: " . $resultado . "<br>";
+?>
